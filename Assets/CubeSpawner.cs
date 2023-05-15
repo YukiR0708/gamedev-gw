2d720cd81db543f738b55fa1f9a181c5ab0a05b8
Ray <rei.piyo.55687@gmail.com>
2023-05-15 17:50:33 +0900

    公文式課題レベル3「インプット」

--- Assets/CubeSpawner.cs
+++ Assets/CubeSpawner.cs
@@ -22,7 +22,7 @@ public class CubeSpawner : MonoBehaviour
         }
     }
 
-    }
+}
 
 
 
--- Assets/Move.cs
+++ Assets/Move.cs
@@ -6,22 +6,45 @@ public class Move : MonoBehaviour
 {
 
     bool _isGoal;
+    public bool IsGoal { get => _isGoal; set => value = _isGoal; }
     Rigidbody _rb;
-    [SerializeField] float _speed = 0.0f;
+    [SerializeField] float _runSpeed = 0.0f;
+    [SerializeField] float _moveSpeed = 0.0f;
+    [SerializeField] Transform _floorTransform = default;
+    Vector3 _defaultPos = default;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _defaultPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!_isGoal)
+        float hInput = Input.GetAxis("Horizontal");
+
+        if (!_isGoal)
         {
-            _rb.velocity = transform.forward * _speed;
+            //強制スクロール
+            _rb.velocity = transform.forward * _runSpeed + Vector3.up * _rb.velocity.y;
+            //左右移動
+            if(hInput != 0)
+            {
+                _rb.velocity += Vector3.right * hInput * _moveSpeed;
+            }
         }
+
+        //落下したら動けなくしてもとの位置に戻る
+        if(transform.position.y < _floorTransform.position.y)
+        {
+            _isGoal = true;
+            transform.position = _defaultPos;
+            _isGoal = false;
+        }
+
+        
     }
 
     private void OnCollisionEnter(Collision collision)
